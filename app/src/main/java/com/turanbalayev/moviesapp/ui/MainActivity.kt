5d5a2878154fdc96dcf8b3e4a68b0e721362bc4e
Turan Balayev <turanbalayev96@gmail.com>
2023-08-04 16:06:12 +0400

    Add essential fragments

--- app/src/main/java/com/turanbalayev/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/turanbalayev/moviesapp/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.turanbalayev.moviesapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.turanbalayev.moviesapp.R
+import com.turanbalayev.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
