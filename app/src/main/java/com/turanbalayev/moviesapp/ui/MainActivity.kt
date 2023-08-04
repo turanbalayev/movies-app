03cac8c0f8fdbeb11fbecbb2524c44f10bc70859
Turan Balayev <turanbalayev96@gmail.com>
2023-08-04 15:47:10 +0400

    Prepare folder structure

--- app/src/main/java/com/turanbalayev/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/turanbalayev/moviesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.turanbalayev.moviesapp
+package com.turanbalayev.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.turanbalayev.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
